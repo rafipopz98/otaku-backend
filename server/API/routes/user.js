95912c7aa4eaa9d6125f9bdfa5a455d6c55eb2bf
rafipopz98 <itsrealrafi@gmail.com>
2023-07-29 23:59:10 +0530

    route user

--- server/API/routes/user.js
+++ server/API/routes/user.js
@@ -1,29 +1,29 @@
-// const express = require("express");
-// const router = express.Router();
-// // user-repository
-// // const {UserRepo} = require("../../database/repository/user");
-// // const UR = new UserRepo();
-// // console.log(UR)
+const express = require("express");
+const router = express.Router();
+// user-repository
+// const {UserRepo} = require("../../database/repository/user");
+// const UR = new UserRepo();
+// console.log(UR)
 
-// // user-service
-// const { UserService } = require("../../services/user");
-// const US = new UserService();
+// user-service
+const { UserService } = require("../../services/user");
+const US = new UserService();
 
-// // /user -> TO GET ALL USERS
-// router.get("/", async (req, res) => {
-//   try {
-//     // success: {success: true, data: {}}
-//     // Failure: {success: false, data: e}
-//     const data = await US.GetAllUsers();
-//     console.log(data);
-//     if (data.success) {
-//       return res.status(200).json(data.data);
-//     }
-//     return res.status(500).json(data.data);
-//   } catch (e) {
-//     console.log("Error while handling get all users", e);
-//     return res.status(500).json({ success: false, data: e });
-//   }
-// });
+// /user -> TO GET ALL USERS
+router.get("/", async (req, res) => {
+  try {
+    // success: {success: true, data: {}}
+    // Failure: {success: false, data: e}
+    const data = await US.GetAllUsers();
+    console.log(data);
+    if (data.success) {
+      return res.status(200).json(data.data);
+    }
+    return res.status(500).json(data.data);
+  } catch (e) {
+    console.log("Error while handling get all users", e);
+    return res.status(500).json({ success: false, data: e });
+  }
+});
 
-// module.exports = router;
+module.exports = router;
